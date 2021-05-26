cdc41849b9f8b932a6d549d4ace163fc5da14d16
Christoph Kretschmer <christoph@kretschmer-und-kretschmer.de>
2021-05-26 21:16:05 +0200

    deleted old client experiments

--- WebClient/src/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import deviceReducer from './deviceReducer.js';
-
-const reducer = combineReducers({
-    device: deviceReducer,
-});
-
-export default reducer;
--- WebClient/src/screens/LoadingScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadingScreen = () => (
-  <div className="loader">
-    <img alt="Loading indicator" className="loader__image" src="/images/loader.gif" />
-  </div>
-);
-
-export default LoadingScreen;
