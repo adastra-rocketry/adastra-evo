56e01d5c1115eaee37614a1530d20961d954efe1
Christoph Kretschmer <christoph@kretschmer-und-kretschmer.de>
2021-05-30 12:07:07 +0200

    global error handler

--- evo_client/android/app/src/main/kotlin/org/adastrarocketry/evo/client/evo_client/MainActivity.kt
+++ evo_client/android/app/src/main/kotlin/org/adastrarocketry/evo/client/evo_client/MainActivity.kt
@@ -1,6 +1,23 @@
 package org.adastrarocketry.evo.client.evo_client
 
 import io.flutter.embedding.android.FlutterActivity
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
+import com.polidea.rxandroidble2.exceptions.BleException
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState);
+        RxJavaPlugins.setErrorHandler { throwable ->
+            if (throwable is UndeliverableException && throwable.cause is BleException) {
+                return@setErrorHandler // ignore BleExceptions since we do not have subscriber
+            }
+            else {
+                throw throwable
+            }
+        }
+    }
 }
